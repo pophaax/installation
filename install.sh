73e75e3eec54a560c891c7767d9184a9ef45e47a
sailbot <sailbot@sailingbot.ax>
2014-06-26 16:44:58 +0300

    Added create and remove daemon

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 #
 
 	# Installation directory
-	INSTALLATION_PATH=~/
+	INSTALLATION_PATH=~/dev/
 	
 	# Main repository
 	REPO_MAIN=sailingrobot
@@ -123,14 +123,48 @@ opt_remove ()
 	printf "$CLR_INFO\nFiles removed\n"
 }
 
+opt_daemon_create ()
+{
+	cd $DIR
+	echo "[Unit]" >> asr.service
+	echo "Description=sailingrobot" >> asr.service
+	echo "Requires=gpsd.service" >> asr.service
+	echo "After=gpsd.service" >> asr.service
+	echo "" >> asr.service
+	echo "[Service]" >> asr.service
+	echo "Type=simple" >> asr.service
+	echo "ExecStart=$INSTALLATION_PATH$REPO_MAIN/sr" >> asr.service
+	echo "RestartSec=5" >> asr.service
+	echo "Restart=on-failure" >> asr.service
+	echo "" >> asr.service
+	echo "[Install]" >> asr.service
+	echo "WantedBy=multi-user.target" >> asr.service
+	printf "$CLR_INFO\nDaemon created\n"
+	sudo mv asr.service /etc/systemd/system/asr.service
+	sudo systemctl enable asr.service
+	rm -rf asr.service
+	printf "$CLR_INFO\nDaemon enabled\n"
+}
+
+opt_daemon_remove ()
+{
+	sudo systemctl disable asr.service
+	printf "$CLR_INFO\nDaemon disabled\n"
+	sudo rm /etc/systemd/system/asr.service
+	printf "$CLR_INFO"
+	printf "Daemon removed\n"
+}
+
 printf "\n$CLR_ASK\nWhat do you want to do?\n$CLR_OPT"
-select option in "Install" "Update" "Remove" "Quit"
+select option in "Install" "Update" "Remove" "Create Daemon" "Remove Daemon" "Quit"
 do
 	case $option in
-	    Install ) opt_install; break;;
-	    Update ) opt_update; break;;
-		Remove ) opt_remove; break;;
-		Quit ) break;;
+		"Install" ) opt_install; break;;
+		"Update" ) opt_update; break;;
+		"Remove" ) opt_remove; break;;
+		"Create Daemon" ) opt_daemon_create; break;;
+		"Remove Daemon" ) opt_daemon_remove; break;;
+		"Quit" ) break;;
 	esac
 done
 
