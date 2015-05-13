6066916fd4211a32778e7075dbcee5b06d96453f
sailbot <sailbot>
2015-05-13 13:09:09 +0300

    added simple shell script for adding symbolic xbee link

---
+++ setupSymlink.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+ 
+
+ sudo mkdir -p /etc/udev/rules.d/
+ sudo echo "SUBSYSTEM==”tty”, ATTRS{idVendor}==0403, ATTRS{idProduct}==6015, ATTRS{serial}==DA013NVR, SYMLINK+=”xbee”" > /etc/udev/rules.d/rules.rules
+
+
