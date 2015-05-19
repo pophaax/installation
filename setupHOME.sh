b87808e43a5c8d01c0fae381616091f493e7b287
sailbot <sailbot>
2015-05-19 09:13:39 +0300

    setupHOME.sh

---
+++ setupHOME.sh
@@ -0,0 +1,14 @@
+ #!/bin/bash
+ 
+
+ ##sudo mkdir -p /bash/bash.bashrc
+echo "Are you running this with sudo?"
+echo "If not you totally should"
+echo >> /etc/bash.bashrc
+echo >> /etc/bash.bashrc
+echo "export SAILINGROBOTS_HOME=/home/sailbot/sailingrobot/\${SAILINGROBOTS_HOME}" >>  /etc/bash.bashrc
+echo >> /etc/bash.bashrc
+echo "Created environment variable: \$SAILINGROBOTS_HOME - please restart terminal before calling it."
+
+
+
