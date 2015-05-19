6efdf11a98f9968ddb96d00b2944a73a61cf7dfc
sailbot <sailbot>
2015-05-19 09:31:37 +0300

    changed typo

--- setupHOME.sh
+++ setupHOME.sh
@@ -6,7 +6,7 @@ echo "Are you running this with sudo?"
 echo "If not you totally should"
 echo >> /etc/bash.bashrc
 echo >> /etc/bash.bashrc
-echo "export SAILINGROBOTS_HOME=/home/sailbot/sailingrobot/\${SAILINGROBOTS_HOME}" >>  /etc/bash.bashrc
+echo "export SAILINGROBOTS_HOME=/home/sailbot/sailingrobot\${SAILINGROBOTS_HOME}" >>  /etc/bash.bashrc
 echo >> /etc/bash.bashrc
 echo "Created environment variable: \$SAILINGROBOTS_HOME - please restart terminal before calling it."
 
