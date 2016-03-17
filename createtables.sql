e1c04ec96d77c12077c2d258c72250e4013f8657
conny <conny.ljunggren@ha.ax>
2016-03-17 11:24:35 +0200

    Fixed issues with the createtables file

--- createtables.sql
+++ createtables.sql
@@ -28,12 +28,12 @@ CREATE TABLE configs (id INTEGER PRIMARY KEY AUTOINCREMENT,
 	flag_heading_compass INTEGER,
 	sr_loop_time DOUBLE,
 	scanning BOOLEAN,
-	wp_inner_radius_ratio DOUBLE
-	sail_adjust_time DOUBLE 
+	wp_inner_radius_ratio DOUBLE,
+	sail_adjust_time DOUBLE, 
 	sail_adjust_degree_limit INTEGER
 );
 INSERT INTO "configs" VALUES(1, 4400,5300, 7000,5520, 45,60, 0.5,5,
-	'/dev/ttyAMA0',4800,'/dev/ttyACM0',4,0,0,3,0,0,0,0,1,0.5,1,0.5, 5.0,10);
+	'/dev/ttyAMA0',4800,'/dev/ttyACM0',4,0,0,3,0,0,0,0,1,0.5,1,0.5,0.5,10);
 
 DROP TABLE IF EXISTS "buffer_configs";
 CREATE TABLE buffer_configs (id INTEGER PRIMARY KEY AUTOINCREMENT,
