d45f8e406a7ddf083a3a14686988b977efbc82ca
Simon Karlsson <simon.karlsson@ha.ax>
2016-04-01 10:56:36 +0000

    Updaterade setupHOME.sh

--- setupHOME.sh
+++ setupHOME.sh
@@ -4,10 +4,10 @@
  ##sudo mkdir -p /bash/bash.bashrc
 echo "Are you running this with sudo?"
 echo "If not you totally should"
-echo >> /etc/bash.bashrc
-echo >> /etc/bash.bashrc
+echo >> /home/sailbot/.bashrc
+echo >> /home/sailbot/.bashrc
 echo "export SAILINGROBOTS_HOME=/home/sailbot/sailingrobot\${SAILINGROBOTS_HOME}" >>  /etc/bash.bashrc
-echo >> /etc/bash.bashrc
+echo >> /home/sailbot/.bashrc
 echo "Created environment variable: \$SAILINGROBOTS_HOME - please restart terminal before calling it."
 
 
