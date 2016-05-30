ae447fc94b8a6fb3db181febe6e4338616f9e91c
Åland Sailing Robots <vilhelm.ostberg@ha.ax>
2016-05-30 10:00:02 +0300

    Update setupSymlink.sh
    correcting udev rules

--- setupSymlink.sh
+++ setupSymlink.sh
@@ -2,6 +2,6 @@
  
 
  sudo mkdir -p /etc/udev/rules.d/
- sudo echo "SUBSYSTEM==”tty”, ATTRS{idVendor}==0403, ATTRS{idProduct}==6015, ATTRS{serial}==DA013NVR, SYMLINK+=”xbee”" > /etc/udev/rules.d/rules.rules
+ sudo echo "SUBSYSTEM==”tty”, ATTRS{idVendor}==”0403”, ATTRS{idProduct}==”6015”, ATTRS{serial}==”DA013NVR”,SYMLINK+=”xbee”" > /etc/udev/rules.d/rules.rules
 
 
