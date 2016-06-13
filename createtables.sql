a34f3ceae49e45479a972745c1da30248620357c
EMIL S <william.soderlund@ha.ax>
2016-06-13 12:36:23 +0300

    Httpsync: new entry
    Added push_only_latest_log

--- createtables.sql
+++ createtables.sql
@@ -264,7 +264,8 @@ DROP TABLE IF EXISTS "httpsync_config";
 CREATE TABLE httpsync_config (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   delay INTEGER,
-  remove_logs BOOLEAN
+  remove_logs BOOLEAN,
+  push_only_latest_logs BOOLEAN
 );
 
 
@@ -325,7 +326,7 @@ INSERT INTO "maestro_controller_config" VALUES(1,'/dev/ttyACM0');
 INSERT INTO "rudder_servo_config" VALUES(1,4,0,0);
 INSERT INTO "sail_servo_config" VALUES(1,3,0,0);
 INSERT INTO "xbee_config" VALUES(1,0,0,0,0.1);
-INSERT INTO "httpsync_config" VALUES(1,0,1);
+INSERT INTO "httpsync_config" VALUES(1,0,1,1);
 INSERT INTO "sailing_robot_config" VALUES(1,1,0.5,0);
 INSERT INTO "waypoint_routing_config" VALUES(1,0.5,7,25,90.0,1.0);
 INSERT INTO "wind_vane_config" VALUES(1,1,0,1.5);
